75c52e1f8f28cb6615818e27a04f0b979f66ee3b
hermawanarby <hermawanbaru4@gmail.com>
2020-04-17 15:49:02 +0700

    koneksi dan tampil data

---
+++ kuliah/pertemuan10/functions.php
@@ -0,0 +1,23 @@
+<?php
+
+function koneksi()
+{
+  return mysqli_connect('localhost', 'root', '', 'pw_193040161');
+}
+
+function query($query)
+{
+  $conn = koneksi();
+  $result = mysqli_query($conn, $query);
+
+  // jika hasilnya hanya 1 data
+  if (mysqli_num_rows($result) == 1) {
+    return mysqli_fetch_assoc($result);
+  }
+
+  $rows = [];
+  while ($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+  }
+  return $rows;
+}
