5f34be0c54eee5ea08bb19898c73fb5fe66b853b
hermawanarby <hermawanbaru4@gmail.com>
2020-05-11 00:31:58 +0700

    merubah tampilan dengan boostrap 4 online

--- praktikum/P7_PW_193040161/latihan7c/php/admin.php
+++ praktikum/P7_PW_193040161/latihan7c/php/admin.php
@@ -19,68 +19,141 @@ if (isset($_GET['cari'])) {
 } else {
   $elektronik = query("SELECT * FROM elektronik");
 }
+$user = query("SELECT * FROM user");
 ?>
 
-<!DOCTYPE html>
+
+<!doctype html>
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Halaman Admin</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <!-- My CSS -->
+  <link rel="stylesheet" href="../css/admin.css">
+  <title>Admin | 193040161</title>
 </head>
 
 <body>
-  <div class="logout">
-    <a href="logout.php">Logout</a>
-  </div>
-  <br>
-  <div class="add">
-    <a href="tambah.php"><button>Tambah Data!</button></a>
-  </div>
-  <br>
-  <form action="" method="get">
-    <input type="text" name="keyword">
-    <button type="submit" name="cari">Cari!</button>
-  </form>
-  <br>
-  <table border="1" cellpadding="13" cellspacing="0">
-    <tr>
-      <th>#</th>
-      <th>Opsi</th>
-      <th>Gambar</th>
-      <th>Nama Produk</th>
-      <th>Kategori</th>
-      <th>Berat</th>
-      <th>Harga</th>
-    </tr>
+  <!-- Navbar -->
+  <nav class="navbar navbar-dark fixed-top" style="background-color: #6c42f5">
+    <a class="navbar-brand" href="#">
+      <i class="fa fa-home"></i>
+      <strong class="text-uppercase"> Halaman Admin</strong>
+    </a>
+    <?php foreach ($user as $u) : ?>
+      <div class="dropdown">
+        <a href="logout.php" class="navbar-brand dropdown-toggle text-capitalize" data-toggle="dropdown"><i class="fa fa-user-circle mr-2"></i>Halo, <?= $u['username']; ?></a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="logout.php"><i class="fa fa-sign-out"></i> Logout</a>
+        </div>
+      </div>
+    <?php endforeach; ?>
+  </nav>
+  <!-- Akhir Navbar -->
 
-    <?php if (empty($elektronik)) : ?>
-      <tr>
-        <td colspan="7">
-          <h1>Data tidak ditemukan</h1>
-        </td>
-      </tr>
-    <?php else : ?>
-      <?php $i = 1; ?>
-      <?php foreach ($elektronik as $e) : ?>
-        <tr>
-          <td><?= $i; ?></td>
-          <td>
-            <a href="ubah.php?id=<?= $e['id']; ?>"><button>Ubah</button></a>
-            <a href="hapus.php?id=<?= $e['id']; ?>" onclick="return confirm('Hapus Data?')"><button>Hapus</button></a>
-          </td>
-          <td><img src="../assets/img/<?= $e['gambar']; ?>" width="100"></td>
-          <td><?= $e['nama_produk']; ?></td>
-          <td><?= $e['kategori']; ?></td>
-          <td><?= $e['berat']; ?></td>
-          <td>Rp.<?= $e['harga']; ?></td>
-        </tr>
-        <?php $i++; ?>
-      <?php endforeach; ?>
-    <?php endif; ?>
+  <div class="row no-gutters mt-5">
+    <div class="col-md-2 bg-dark mt-2 pr-3 pt-4" id="sidebar">
+      <ul class="nav flex-column sidenav ml-3 mb-5">
+        <li class="nav-item">
+          <a class="nav-link active text-white" href="#"><i class="fa fa-tachometer mr-2"></i> Dashboard</a>
+          <hr class="bg-secondary">
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="admin.php"><i class="fa fa-tags mr-2"></i> Daftar Produk</a>
+          <hr class="bg-secondary">
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#"><i class="fa fa-users mr-2"></i> Daftar Pengguna</a>
+          <hr class="bg-secondary">
+        </li>
+        <li class="nav-item text-white fixed-bottom ml-3">
+          <p>&copy; 2020 | Hermawan Arby</p>
+        </li>
+      </ul>
+    </div>
+    <div class="col-md-10 p-5 pt-2" id="main">
+      <h3 class="text-uppercase"><i class="fa fa-tags mr-2 "></i> Daftar Produk</a></h3>
+      <hr>
+      <div class="row mb-3">
+        <div class="col">
+          <div class="input-group">
+            <a href="tambah.php" class="btn text-white" style="background-color: #6c42f5"><i class="fa fa-plus-circle mr-2"></i>Tambah Data!</a>
+          </div>
+        </div>
+        <div class="col-lg-5">
+          <div class="input-group">
+            <form action="" method="get" class="input-group">
+              <input type="text" class="form-control" name="keyword" placeholder="Masukan keyword pencarian.." autocomplete="off" autofocus>
+              <div class="input-group-append">
+                <button class="btn text-white" type="submit" name="cari" style="background-color: #6c42f5">Search!</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <table class="table table-bordered">
+        <thead class="text-center text-uppercase bg-dark text-white">
+          <tr>
+            <th scope="col">NO</th>
+            <th scope="col">Gambar</th>
+            <th scope="col">Nama Produk</th>
+            <th scope="col">Kategori</th>
+            <th scope="col">Berat</th>
+            <th scope="col">Harga</th>
+            <th scope="col" width="200">Aksi</th>
+          </tr>
+        </thead>
 
-  </table>
+        <?php if (empty($elektronik)) : ?>
+          <tr class="text-center">
+            <td colspan="7">
+              <h1>404 Not Found!</h1>
+              <hr style="max-width: 400px;">
+              <p>Data produk tidak ditemukan!</p>
+            </td>
+          </tr>
+        <?php else : ?>
+
+          <?php $i = 1; ?>
+          <?php foreach ($elektronik as $e) : ?>
+            <?php $angka = $e['harga']; ?>
+            <tbody>
+
+              <tr>
+                <th scope="row"><?= $i; ?></th>
+                <td><img src="../assets/images/<?= $e['gambar']; ?>" width="100"></td>
+                <td width="300"><?= $e['nama_produk']; ?></td>
+                <td><?= $e['kategori']; ?></td>
+                <td><?= $e['berat']; ?></td>
+                <td><?= 'Rp ' . number_format($angka, 0, ".", "."); ?></td>
+                <td>
+                  <a href="ubah.php?id=<?= $e['id']; ?>" class="btn btn-success btn-sm text-white" data-toggle="tooltip" title="Edit"><i class="fa fa-edit"></i> Edit</a>
+                  <a href="hapus.php?id=<?= $e['id']; ?>" class="btn btn-danger btn-sm text-white" onclick="return confirm('Hapus Data?')" data-toggle="tooltip" title="Delete"><i class="fa fa-trash"></i> Delete</a>
+                </td>
+              </tr>
+            </tbody>
+            <?php $i++; ?>
+          <?php endforeach; ?>
+        <?php endif; ?>
+      </table>
+    </div>
+  </div>
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  <script src="../js/admin.js"></script>
 </body>
 
 </html>
--- praktikum/P7_PW_193040161/latihan7c/php/ubah.php
+++ praktikum/P7_PW_193040161/latihan7c/php/ubah.php
@@ -29,47 +29,98 @@ if (isset($_POST['ubah'])) {
 }
 
 ?>
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Ubah Data</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <!-- My CSS -->
+  <link rel="stylesheet" href="../css/admin.css">
+  <title>Admin | 193040161</title>
 </head>
 
 <body>
-  <h3>Form Ubah Data Barang</h3>
-  <form action="" method="post">
-    <input type="hidden" name="id" id="id" value="<?= $elektronik['id']; ?>">
-    <ul>
-      <li>
-        <label for="nama_produk">Nama Produk</label><br>
-        <input type="text" name="nama_produk" id="nama_produk" value="<?= $elektronik['nama_produk']; ?>" required><br><br>
-      </li>
-      <li>
-        <label for="kategori">Kategori</label><br>
-        <input type="text" name="kategori" id="kategori" value="<?= $elektronik['kategori']; ?>" required><br><br>
-      </li>
-      <li>
-        <label for="berat">Berat</label><br>
-        <input type="text" name="berat" id="berat" value="<?= $elektronik['berat']; ?>" required><br><br>
-      </li>
-      <li>
-        <label for="harga">Harga</label><br>
-        <input type="number" name="harga" id="harga" value="<?= $elektronik['harga']; ?>" required><br><br>
-      </li>
-      <li>
-        <label for="gambar">Gambar</label><br>
-        <input type="text" name="gambar" id="gambar" value="<?= $elektronik['gambar']; ?>" required><br><br>
-      </li>
-      <br>
-      <button type="submit" name="ubah">Ubah Data!</button>
-      <button type="button">
-        <a href="admin.php" style="text-decoration: none; color: black;">Kembali</a>
-      </button>
-    </ul>
-  </form>
+  <!-- Navbar -->
+  <nav class="navbar navbar-dark fixed-top" style="background-color: #6c42f5">
+    <a class="navbar-brand" href="#">
+      <img src="/docs/4.4/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+      <strong class="text-uppercase"><i class="fa fa-home"></i> Halaman Admin</strong>
+    </a>
+  </nav>
+  <!-- Akhir Navbar -->
+
+  <div class="row no-gutters mt-5">
+    <div class="col-md-2 bg-dark mt-2 pr-3 pt-4" id="sidebar">
+      <ul class="nav flex-column sidenav ml-3 mb-5">
+        <li class="nav-item">
+          <a class="nav-link active text-white" href="#"><i class="fa fa-tachometer mr-2"></i> Dashboard</a>
+          <hr class="bg-secondary">
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="admin.php"><i class="fa fa-tags mr-2"></i> Daftar Produk</a>
+          <hr class="bg-secondary">
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#"><i class="fa fa-users mr-2"></i> Daftar Pengguna</a>
+          <hr class="bg-secondary">
+        </li>
+        <li class="nav-item text-white fixed-bottom ml-3">
+          <p>&copy; 2020 | Hermawan Arby</p>
+        </li>
+      </ul>
+    </div>
+    <div class="col-md-6 p-5 pt-2" id="main">
+      <h3 class="text-uppercase"><i class="fa fa-edit"></i> Edit Data</a></h3>
+      <hr>
+      <div class="card">
+        <div class="card-header text-white" style="background-color: #6c42f5">
+          Form Edit Data Barang Elektronik
+        </div>
+        <div class="card-body">
+          <form action="" method="post">
+            <input type="hidden" name="id" id="id" value="<?= $elektronik['id']; ?>">
+            <div class="form-group">
+              <label for="nama_produk">Nama Produk:</label>
+              <input type="text" class="form-control" name="nama_produk" id="nama_produk" value="<?= $elektronik['nama_produk']; ?>" required>
+            </div>
+            <div class="form-group">
+              <label for="kategori">Kategori:</label>
+              <input type="text" class="form-control" name="kategori" id="kategori" value="<?= $elektronik['kategori']; ?>" required>
+            </div>
+            <div class="form-group">
+              <label for="berat">Berat:</label>
+              <input type="text" class="form-control" name="berat" id="berat" value="<?= $elektronik['berat']; ?>" required>
+            </div>
+            <div class="form-group">
+              <label for="harga">Harga:</label>
+              <input type="number" class="form-control" name="harga" id="harga" value="<?= $elektronik['harga']; ?>" required>
+            </div>
+            <div class="form-group">
+              <label for="gambar">Gambar:</label>
+              <input type="text" class="form-control" name="gambar" id="gambar" value="<?= $elektronik['gambar']; ?>" required>
+            </div>
+            <a href="admin.php" class="btn btn-secondary float-right">Kembali</a>
+            <button type="submit" name="ubah" class="btn text-white float-right mr-2" style="background-color: #6c42f5">Edit</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  <script src="../js/admin.js"></script>
 </body>
 
 </html>
