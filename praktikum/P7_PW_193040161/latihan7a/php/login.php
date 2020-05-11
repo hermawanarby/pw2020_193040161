6b013573b3c4e6ef2284e9c2cfbefcccaea15685
hermawanarby <hermawanbaru4@gmail.com>
2020-05-11 21:41:13 +0700

    merubah tampilan dengan boostrap 4 online

--- praktikum/P7_PW_193040161/latihan7a/php/login.php
+++ praktikum/P7_PW_193040161/latihan7a/php/login.php
@@ -30,37 +30,83 @@ if (isset($_POST['submit'])) {
 
 ?>
 
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Login</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+  <title>Login - 193040161</title>
+
+  <style>
+    body {
+      background-image: url('../assets/images/bg.jpg');
+      background-size: cover;
+      font-family: 'Roboto', sans-serif;
+    }
+
+    .card {
+      width: 350px;
+      box-shadow: 1px 1px 20px 5px rgba(0, 0, 0, 0.1);
+    }
+
+    .card-header,
+    button {
+      background-color: #6c42f5 !important;
+    }
+  </style>
 </head>
 
 <body>
-  <?php if (isset($error)) : ?>
-    <p style="color: red; font-style: italic;">Username / Password Salah!</p>
-  <?php endif; ?>
-  <form action="" method="post">
-    <table>
-      <tr>
-        <td><label for="username">Username</label></td>
-        <td>:</td>
-        <td><input type="text" name="username"></td>
-      </tr>
-      <tr>
-        <td><label for="password">Password</label></td>
-        <td>:</td>
-        <td><input type="password" name="password"></td>
-      </tr>
-    </table>
-    <input type="checkbox" name="remember">
-    <label for="remember">Remember Me</label>
-    <br>
-    <button type="submit" name="submit">Login</button>
-  </form>
+
+  <div class="container vh-100">
+    <div class="row justify-content-center h-100">
+      <div class="card my-auto">
+        <div class="card-header text-center text-white">
+          <h2>Login Form</h2>
+        </div>
+        <div class="card-body">
+          <?php if (isset($error)) : ?>
+            <div class="alert alert-danger text-center" role="alert">
+              Username / Password Salah!
+            </div>
+          <?php endif; ?>
+          <form action="" method="post">
+            <div class="form-group">
+              <label for="username">Username:</label>
+              <input type="text" class="form-control" name="username" id="username">
+            </div>
+            <div class="form-group">
+              <label for="password">Password:</label>
+              <input type="password" class="form-control" name="password" id="password">
+            </div>
+            <div class="custom-control custom-checkbox mb-3">
+              <input type="checkbox" class="custom-control-input" name="remember" id="customCheck1">
+              <label class="custom-control-label" for="customCheck1">Remember Me</label>
+            </div>
+            <button type="submit" class="btn btn-primary w-100" name="submit">Login</button>
+          </form>
+        </div>
+        <div class="card-footer text-center">
+          <small>&copy; 2020 eShop.</small>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+  </script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+  </script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+  </script>
 </body>
 
 </html>
