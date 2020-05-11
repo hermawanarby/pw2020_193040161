88d0fb946b4d8c64a75ecb20ff80ddace0bece3d
hermawanarby <hermawanbaru4@gmail.com>
2020-05-12 02:15:16 +0700

    update database

--- praktikum/P7_PW_193040161/tubes_193040161.sql
+++ praktikum/P7_PW_193040161/tubes_193040161.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 03, 2020 at 06:57 PM
+-- Generation Time: May 11, 2020 at 09:12 PM
 -- Server version: 10.4.8-MariaDB
 -- PHP Version: 7.3.11
 
@@ -42,16 +42,16 @@ CREATE TABLE `elektronik` (
 --
 
 INSERT INTO `elektronik` (`id`, `nama_produk`, `kategori`, `berat`, `harga`, `gambar`) VALUES
-(1, 'SHARP LC-24SA4000i Aquos Televisi LED 24 Inch', 'TV &amp; Aksesoris', '8 Kg', 1390000, '1.jpg'),
-(2, 'Receiver Tanaka New Samurai Tanberg Edition', 'TV & Aksesoris', '650 gr', 155000, '2.jpg'),
-(3, 'JBL Go 2 - Black', 'Audio', '500 gr', 849000, '3.jpg'),
-(4, 'Mic Recorder Condenser Laptop Karaoke Smule BM800 Microphone Condensor', 'Audio', '300 gr', 138000, '4.jpg'),
-(5, 'Printer Canon MG 2570s Print Scan Copy', 'Printer', '6 Kg', 605000, '5.jpg'),
-(6, 'Epson Printer L3110 Print Scan Copy - Hitam', 'Printer', '8 Kg', 2035000, '6.jpg'),
-(7, 'COSMOS Kipas Angin Berdiri 16\" - 16-XDC', 'Pendingin Ruangan', '10 Kg', 202000, '7.png'),
-(8, 'Midea MSBC-05CRN1 AC Split 0.5 PK Standard', 'Pendingin Ruangan', '75 Kg', 2374000, '8.jpg'),
-(9, 'PHILIPS Lampu LED MyCare 3W Kuning Bohlam LED Bulb My Care 3 Watt WW', 'Lampu', '60 gr', 16800, '9.png'),
-(10, 'Senter Chip LED Torch Cree E17 XM-L T6 Taffware Magnet COB Zoom 2019', 'Lampu', '200 gr', 47000, '10.jpg');
+(1, 'TOSHIBA 43 Inch Pro Theatre Series Smart TV UHD 43U6750', 'TV &amp; Aksesoris', '8 Kg', 6975000, '1.jpg'),
+(2, 'MXQ Pro 4K Kodi Android Smart OTT Internet TV Box Media Player S905X', 'TV & Aksesoris', '650 gr', 750000, '2.jpg'),
+(3, 'GMC Speaker Multimedia 886J Black Light Blue', 'Audio', '900 gr', 1083000, '3.jpg'),
+(4, 'POLYTRON Speaker Aktif PAS 27', 'Audio', '3 Kg', 138000, '4.jpg'),
+(5, 'HP Smart Tank 515 All-in-One Printer', 'Printer', '6 Kg', 2699000, '5.jpg'),
+(6, 'CANON PIXMA E410 - Black', 'Printer', '8 Kg', 944000, '6.jpg'),
+(7, 'SANKEN Kipas Angin FS-735TWH', 'Pendingin Ruangan', '10 Kg', 355000, '7.jpg'),
+(8, 'SAMSUNG AC Split AR07JRSDUWKNSE', 'Pendingin Ruangan', '75 Kg', 5434000, '8.jpg'),
+(9, 'Q2 Lampu Meja 8901 Red', 'Lampu', '500 gr', 75000, '9.jpg'),
+(10, 'ANKER Flashlight Super Bright Senter 900 Lumens LC90 [T1420H11] (M)', 'Lampu', '200 gr', 799000, '10.jpg');
 
 -- --------------------------------------------------------
 
@@ -70,7 +70,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `username`, `password`) VALUES
-(5, 'arby', '$2y$10$.T4asVY4sSYa0NkgZhXD.eFGZ1rBesU4k5irFDTvvmr7wrNOeFlAG');
+(5, 'arby', '$2y$10$.T4asVY4sSYa0NkgZhXD.eFGZ1rBesU4k5irFDTvvmr7wrNOeFlAG'),
+(9, 'admin', '$2y$10$1Y.bDi.fdwYu8oPlWG4i.ODwmVuya2kBpsyWnMTjijTBvucOc6O7e');
 
 --
 -- Indexes for dumped tables
@@ -96,13 +97,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `elektronik`
 --
 ALTER TABLE `elektronik`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
