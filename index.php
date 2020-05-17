668ee2d6131d172393a2bb8105bf6ff6dc0e488e
hermawanarby <hermawanbaru4@gmail.com>
2020-05-18 00:06:31 +0700

    membuat index main

---
+++ index.php
@@ -0,0 +1,158 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="icon" type="image/png" href="assets/logo.png">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css2?family=Lobster&family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+
+  <!-- My CSS -->
+  <style>
+    body {
+      font-family: 'Roboto', sans-serif;
+    }
+
+    .navbar-brand {
+      font-family: 'Lobster', cursive, sans-serif;
+      font-size: 2em;
+    }
+
+    .navbar-nav {
+      font-family: 'Roboto', sans-serif;
+    }
+
+    .jumbotron {
+      margin-top: 70px;
+      height: 570px;
+      background-image: url('assets/bg-jumbotron.jpg');
+      background-size: cover;
+    }
+
+    .display-4 {
+      font-family: 'Roboto', sans-serif;
+      font-weight: 500;
+      padding-top: 150px;
+      text-shadow: 0px 1px 8px 1px rgba(0, 0, 0, .09);
+    }
+
+    h1 {
+      font-family: 'Roboto', sans-serif;
+      font-weight: 500;
+    }
+
+    .card-body {
+      height: 350px;
+    }
+
+    .footer {
+      padding-top: 30px;
+      padding-bottom: 10px;
+    }
+  </style>
+
+  <title>Index - 193040161</title>
+</head>
+
+<body>
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">ARBY</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse " id="navbarNavAltMarkup">
+        <div class="navbar-nav ml-auto">
+          <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link" href="#tugas">List Tugas</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+  <!-- Akhir Navbar -->
+
+  <!-- Jumbotron -->
+  <div class="jumbotron jumbotron-fluid">
+    <div class="container text-white text-center">
+      <h1 class="display-4">Welcome to Home</h1>
+      <p class="lead">Website untuk menyimpan tugas-tugas Pemrograman Web.</p>
+    </div>
+  </div>
+  <!-- Akhir Jumbotron -->
+
+  <!-- List Tugas -->
+  <section id="tugas">
+    <div class="container">
+      <h2 class="text-center text-uppercase mb-4">List Tugas</h2>
+      <hr>
+      <div class="row">
+        <div class="col-12 col-md-6 col-lg-4">
+          <div class="card mb-3">
+            <div class="card-header text-white text-center text-uppercase bg-dark">
+              <h4>Kuliah</h4>
+            </div>
+            <div class="card-body">
+              <ul class="list-group">
+                <li class="list-group-item"><a href="kuliah/pertemuan9" class="btn btn-secondary btn-block" target="_blank">Pertemuan 9</a></li>
+                <li class="list-group-item"><a href="kuliah/pertemuan10/main.php" class="btn btn-secondary btn-block" target="_blank">Pertemuan 10</a></li>
+                <li class="list-group-item"><a href="kuliah/pertemuan11/" class="btn btn-secondary btn-block" target="_blank">Pertemuan 11</a></li>
+                <li class="list-group-item"><a href="kuliah/pertemuan12/" class="btn btn-secondary btn-block" target="_blank">Pertemuan 12</a></li>
+                <li class="list-group-item"><a href="kuliah/pertemuan13/" class="btn btn-secondary btn-block" target="_blank">Pertemuan 13</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-4">
+          <div class="card mb-3">
+            <div class="card-header text-white text-center text-uppercase bg-dark">
+              <h4>Praktikum</h4>
+            </div>
+            <div class="card-body">
+              <ul class="list-group">
+                <li class="list-group-item"><a href="praktikum/P5_PW_193040161/" class="btn btn-secondary btn-block" target="_blank">P5_PW_193040161</a></li>
+                <li class="list-group-item"><a href="praktikum/P6_PW_193040161/" class="btn btn-secondary btn-block" target="_blank">P6_PW_193040161</a></li>
+                <li class="list-group-item"><a href="praktikum/P7_PW_193040161/" class="btn btn-secondary btn-block" target="_blank">P7_PW_193040161</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-4">
+          <div class="card mb-3">
+            <div class="card-header text-white text-center text-uppercase bg-dark">
+              <h4>Tubes</h4>
+            </div>
+            <div class="card-body">
+              <ul class="list-group">
+                <li class="list-group-item"><a href="tubes/" class="btn btn-secondary btn-block" target="_blank">TUBES</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Akhir List Tugas -->
+
+  <!-- Footer -->
+  <section class="bg-dark mt-5">
+    <div class="footer text-white text-center">
+      <p>&copy; Copyright 2020. Design by <a href="#" class="text-white">Hermawan Arby</a></p>
+    </div>
+  </section>
+  <!-- Akhir Footer -->
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ kuliah/pertemuan10/main.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="icon" type="image/png" href="../../assets/logo.png">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css2?family=Lobster&family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+
+  <!-- My CSS -->
+  <style>
+    body {
+      font-family: 'Roboto', sans-serif;
+    }
+
+    .navbar-brand {
+      font-family: 'Lobster', cursive, sans-serif;
+      font-size: 2em;
+    }
+
+    .tugas {
+      padding-top: 150px;
+    }
+  </style>
+
+  <title>Index - 193040161</title>
+</head>
+
+<body>
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand mx-auto" href="#">ARBY</a>
+    </div>
+  </nav>
+  <!-- Akhir Navbar -->
+
+  <!-- List Tugas -->
+  <section id="tugas" class="tugas">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-12 col-md-6 col-lg-4">
+          <div class="card mb-3 ">
+            <div class="card-header text-white text-center text-uppercase bg-dark">
+              <h4>Petemuan 10</h4>
+            </div>
+            <div class="card-body">
+              <ul class="list-group">
+                <li class="list-group-item"><a href="latihan1.php" class="btn btn-secondary btn-block">Latihan 1</a></li>
+                <li class="list-group-item"><a href="latihan2.php" class="btn btn-secondary btn-block">Latihan 2</a></li>
+                <li class="list-group-item"><a href="latihan3.php" class="btn btn-secondary btn-block">Latihan 3</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+  <!-- Akhir List Tugas -->
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+
+</html>
